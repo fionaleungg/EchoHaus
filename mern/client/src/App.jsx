02b36b275f054c474dfb51e24272ee0f0bf55f27
mizwan <wanmichelle0@gmail.com>
2025-04-26 08:18:13 -0700

    single note

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './signup-login/Signup'
 import Login from './signup-login/Login'
 import NoteList from './notes/NoteList'
 import UploadNotes from './notes/UploadNotes'
+import SingleNote from './notes/SingleNote'
 
 function App() {
 
@@ -16,7 +17,8 @@ function App() {
       {/* <Landing /> */}
       {/* <Signup /> */}
       {/* <Login /> */}
-      <NoteList />
+      {/* <NoteList /> */}
+      <SingleNote />
       {/* <UploadNotes/> */}
       {/* <p className="read-the-docs">
       </p> */}
