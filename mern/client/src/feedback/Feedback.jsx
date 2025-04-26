adc7c2bd780a1d3f88ec1e9900b96609cdd6c05e
fionaleungg <fionaleung1134@gmail.com>
2025-04-26 15:15:59 -0700

    feedback buttons

--- mern/client/src/feedback/Feedback.jsx
+++ mern/client/src/feedback/Feedback.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from '../styles/Feedback.module.css'
+import LogoutButton from '../signup-login/LogoutButton';
+import {useNavigate, useLocation} from 'react-router-dom';
 
 function Feedback() {
+  const navigate = useNavigate();
   return (
     <div className = {styles.feedback}>
       <div className = {styles.container}>
@@ -12,10 +15,11 @@ function Feedback() {
             <div className={styles.text}>feedback from gemini goes here</div>
         </div>
         </div>
-        {/* <LogoutButton /> */}
-        <button className={styles.repeatButton}>
-          Repeat
-        </button>
+        <LogoutButton />
+        <div className = {styles.buttons}>
+            <button className={styles.button} onClick={() => navigate("/intermission")}>Repeat</button>
+            <button className={styles.button} onClick={() => navigate("/study")}>I need to study more</button>
+          </div>
       </div>
     </div>
   )
