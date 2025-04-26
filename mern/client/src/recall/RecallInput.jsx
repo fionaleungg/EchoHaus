0f07b2919d20ca05a0028b1dab5866ecfcc5f04f
fionaleungg <fionaleung1134@gmail.com>
2025-04-26 13:44:18 -0700

    recall and more timer styling

--- mern/client/src/recall/RecallInput.jsx
+++ mern/client/src/recall/RecallInput.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import RecallContext from './RecallContext'
+import styles from '../styles/RecallInput.module.css'
 
 function RecallInput() {
   const rtx = React.useContext(RecallContext);
   const [curInput, setCurInput] = React.useState("");
   return (
-    <>
-      <input type="text" onChange={(e) => setCurInput(e.target.value)} />
-      <button onClick={() => rtx.setCurrentRecall(curInput)}>Submit Recall</button>
-    </>
+    // <div className = {styles.recallInput}>
+    //   <input type="text" onChange={(e) => setCurInput(e.target.value)} />
+    //   <button onClick={() => rtx.setCurrentRecall(curInput)}>Submit Recall</button>
+    // </div>
+    <div className = {styles.recallInput}>
+      <div className = {styles.container}>
+        <h2 className={styles.phase}>Phase 3</h2>
+        <h1 className={styles.title}>Recall</h1>
+        <div className={styles.notebook}>
+          <textarea className = {styles.inputfield} type="text"
+          onChange={(e) => setCurInput(e.target.value)}
+          placeholder = "Recall and summarize your understanding here"/>
+        </div>
+        {/* <LogoutButton /> */}
+        <button className={styles.submitButton} onClick={() => rtx.setCurrentRecall(curInput)}>Submit Recall</button>
+      </div>
+    </div>
   )
 }
 
